4c341a81554c77c6e1af59d341dc1d5f9ecc49ea
Abid Hasan Rana <abidhasanrana650@gmail.com>
2026-05-16 17:33:33 +0600

    50A

---
+++ Day-2/1st.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL);
+inline ceil_div(long long  a, long long  b) {
+    return (a + b - 1) / b;
+}
+inline void toLowerCase(string &s)
+{
+    transform(s.begin(), s.end(), s.begin(), ::/*toupper*/tolower);
+}
+ 
+void solve() 
+{
+ 
+int a , b ; 
+cin>> a >> b; 
+ 
+int ans =(a*b)/2;
+cout<<ans <<nl;
+   
+}
+ 
+int main() {
+    FAST_IO
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+ 
+    return 0;
+}
