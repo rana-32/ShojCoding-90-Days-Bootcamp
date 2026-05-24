4dd78cbd81912579bc4cec6bd906ff565f3e423c
Abid Hasan Rana <abidhasanrana650@gmail.com>
2026-05-24 23:53:44 +0600

    617A

---
+++ Day-3/3rd.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL);
+inline ceil_div(long long  a, long long  b) {
+    return (a + b - 1) / b;
+}
+inline void toLowerCase(string &s)
+{
+    transform(s.begin(), s.end(), s.begin(), ::/*toupper*/tolower);
+}
+
+void solve() 
+{
+ 
+int a ; cin>> a ;
+
+int b = 5 ;
+
+cout<< ( a + b -1)/b <<nl;
+
+
+
+
+    
+}
+
+int main() {
+    FAST_IO
+
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
